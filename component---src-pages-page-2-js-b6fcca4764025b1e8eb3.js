8717fd857cc386ab108efd37f20cbfa6d47d55b3
falexandre <fabiolalexandre@gmail.com>
2019-10-06 11:24:32 -0300

    Updates

---
+++ component---src-pages-page-2-js-b6fcca4764025b1e8eb3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{252:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(95),c=a(250),r=a(249);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-b6fcca4764025b1e8eb3.js.map
